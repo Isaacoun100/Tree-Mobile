b16faab16c17407c689e70b23853b8eaad6877fb
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-07-28 21:48:43 -0600

    Tree implemented

--- MobileTree/MobileTree/Data_Structures/BinaryTree.cs
+++ MobileTree/MobileTree/Data_Structures/BinaryTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MobileTree.Data_Structures
@@ -40,11 +41,103 @@ namespace MobileTree.Data_Structures
             return this.Find(value, this.Root);
         }
 
-        public Node Find(int value, Node root)
+        private Node Find(int value, Node root)
         {
-            if()
+            if(root.Data == value) return root;
+            if(root.Right != null) Find(value, root.Right);
+            if(root.Left != null) Find(value, root.Left);
+            return null;
         }
 
+        private int MinValue(Node node)
+        {
+            int minv = node.Data;
+ 
+            while (node.LeftNode != null)
+            {
+                minv = node.LeftNode.Data;
+                node = node.LeftNode;
+            }
+
+            return minv;
+        }
+
+        public void Remove(int value)
+        {
+            this.Root = Remove(this.Root, value);
+        }
+
+        private Node Remove(Node parent, int key)
+        {
+
+            if(parent==null) return parent;
+
+            if(key<parent.Data) parent.Left = Remove(parent.Left, key);
+            else if (key>parent.Data) parent.Right = Remove(parent.Right, key);
+            else
+            {
+                if (parent.LeftNode == null) return parent.RightNode;
+                else if (parent.RightNode == null) return parent.LeftNode;
+
+                parent.Data=MinValue(parent.Right);
+                parent.Right=Remove(parent.Right, parent.Data);
+            }
+            return parent;
+        }
+
+        public String TraversePreOrder(Node parent)
+        {
+
+            return (TraversePreOrder(parent, "["))+"]";
+
+        }
+
+        private String TraversePreOrder(Node parent, String result)
+        {
+            if (parent != null)
+            {
+                result+=parent.Data + "-->";
+                result+=TraversePreOrder(parent.LeftNode, result);
+                result+=TraversePreOrder(parent.RightNode, result);
+            }
+            return result;
+        }
+
+        public String TraverseInOrder(Node parent)
+        {
+
+            return (TraverseInOrder(parent, "["))+"]";
+
+        }
+
+        private String TraverseInOrder(Node parent, String result)
+        {
+            if (parent != null)
+            {
+                result+=TraverseInOrder(parent.LeftNode);
+                result+=parent.Data + "-->";
+                result+=TraverseInOrder(parent.RightNode);
+            }
+
+            return result;
+        }
+
+        public String TraversePostOrder(Node parent)
+        {
+
+            return (TraversePostOrder(parent, "["))+"]";
+
+        }
+
+        private void TraversePostOrder(Node parent, String result)
+        {
+            if (parent != null)
+            {
+                result+=TraversePostOrder(parent.LeftNode);
+                result+=TraversePostOrder(parent.RightNode);
+                result+=parent.Data + "-->";
+            }
+        }
 
     }
 }
